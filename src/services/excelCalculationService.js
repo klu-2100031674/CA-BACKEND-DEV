8a49bd5d81534be30ab42b82671c997b4c9a7291
jithendrasairamesh <mattajithendra07@gmail.com>
2025-11-18 18:07:14 +0530

    update dir in temp

--- src/services/excelCalculationService.js
+++ src/services/excelCalculationService.js
@@ -27,7 +27,7 @@ class ExcelCalculationService {
     this.pythonExecutable = fs.existsSync(venvPythonPath) ? venvPythonPath : 'python';
     
     console.log(`[ExcelCalculationService] Python executable path: ${this.pythonExecutable}`);
-    this.tempDir = process.env.TEMP_DIR || path.join(__dirname, '../../temp');
+    this.tempDir = process.env.TEMP_DIR;
     console.log(`[ExcelCalculationService] Temp directory: ${this.tempDir}`);
   }
 
